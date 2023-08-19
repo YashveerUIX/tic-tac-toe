91b3e74369a97841075b05d6e7f6980460555b89
Yashveer Singh <yashveer@uixlabs.in>
2023-08-19 20:06:51 +0530

    define start and click functions

--- index.js
+++ index.js
@@ -10,3 +10,46 @@ const WINNING_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+
+const cellElements = document.querySelectorAll("[data-cell]");
+const container = document.getElementById("container");
+const winningMessageElement = document.getElementById("msg");
+const resetButton = document.getElementById("reset");
+const winMsg = document.querySelector("[data-msg-text]");
+let circleTurn;
+
+startGame();
+
+resetButton.addEventListener("click", startGame);
+
+function startGame() {
+  circleTurn = false;
+  cellElements.forEach((cell) => {
+    cell.classList.remove(xClass);
+    cell.classList.remove(cClass);
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setHoverClass();
+  winningMessageElement.classList.remove("show");
+}
+
+function handleClick(e) {
+  const cell = e.target;
+  const currentClass = circleTurn ? cClass : xClass;
+  placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+    setBoardHoverClass();
+  }
+}
+
+// function placeMark() {}
+// function swapTurns() {}
+// function isDraw() {}
+// function endGame() {}
+// function setHoverClass() {}
